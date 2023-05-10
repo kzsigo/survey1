ef888edf926821430a445b08f86fba13aa5fbba5
Leonidas Caldwell <leonidascaldwell@gmail.com>
2023-05-10 00:59:39 -0400

    Revert "UI updates"
    This reverts commit 5f45344840ca5116067751c534abb4923eb70d1d.

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -8,7 +8,149 @@ import "react-multi-carousel/lib/styles.css";
 
 export default function Results({ SearchID, runResponseList, analysisResponse, token, scrollFunction }) {
 
+    let resultsList = []
+    async function searchRunResponse(requestBody, token) {
+        const response = await axios.post
+            (
+                        /*Request URL*/'https://ss.prestoapi.com/api/v1_searchrun',
+                        /*Request Body*/requestBody,
+                        /*Headers*/{
+                    headers:
+                    {
+                        'Authorization': 'Bearer ' + token,
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+        return await response.data
+    }   
+
     
+    /*useEffect(() => {
+        async function getRunResults() {
+            try {
+                if (runResponseList.length > 0) {
+                    //make the rest of the calls
+                    console.log('runResponseList', runResponseList                    )
+                    for await (const request of runResponseList) {
+                        console.log('Iterarting Request', request)
+                        const response = await searchRunResponse(request, token)
+                        console.log('search run response', response)
+
+                        await resultsList.push({response})
+
+                    }
+                    
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        getRunResults();
+        loading = true
+    }
+
+        , [resultsList]);*/
+
+    /*const addJSON = [
+        {
+            "SearchID": 1294,
+            "MaxRows": 3
+        }
+    ]
+    const runResponseList = [
+        {
+            "StrainID": 67,
+            "StrainTypeID": 1,
+            "StrainType": "Flower",
+            "Strain": "Cookies and Cream Bud 1",
+            "SpeciesID": 1,
+            "Species": "Sativa",
+            "DominanceID": 1,
+            "Dominance": "THC Dominant",
+            "TotalTHC": 0.2535,
+            "TotalCBD": 0.0,
+            "StrainScore": 188.0,
+            "InStock": 1,
+            "Price": 0.00,
+            "Unit": "unspecified"
+        },
+        {
+            "StrainID": 76,
+            "StrainTypeID": 1,
+            "StrainType": "Flower",
+            "Strain": "Jesus OG 1",
+            "SpeciesID": 1,
+            "Species": "Sativa",
+            "DominanceID": 1,
+            "Dominance": "THC Dominant",
+            "TotalTHC": 0.3132,
+            "TotalCBD": 0.0,
+            "StrainScore": 172.0,
+            "InStock": 0,
+            "Price": 0.00,
+            "Unit": "unspecified"
+        },
+        {
+            "StrainID": 64,
+            "StrainTypeID": 1,
+            "StrainType": "Flower",
+            "Strain": "Platinum Banana OG",
+            "SpeciesID": 1,
+            "Species": "Sativa",
+            "DominanceID": 1,
+            "Dominance": "THC Dominant",
+            "TotalTHC": 0.2423,
+            "TotalCBD": 0.0,
+            "StrainScore": 142.0,
+            "InStock": 1,
+            "Price": 0.00,
+            "Unit": "unspecified"
+        }
+    ]
+    const analysisResponse = [
+        {
+            "Category": "Property",
+            "TC": "Terpene",
+            "TerpeneCannabinoid": "Endo-(+)-Fenchyl Alcohol",
+            "Property": "Anti-Microbial",
+            "Score": 0.041999999999999996,
+            "Percentage": 0.0006
+        },
+        {
+            "Category": "Property",
+            "TC": "Terpene",
+            "TerpeneCannabinoid": "Alpha Bisabolol",
+            "Property": "Anti-Microbial",
+            "Score": 0.028,
+            "Percentage": 0.0004
+        },
+        {
+            "Category": "Property",
+            "TC": "Terpene",
+            "TerpeneCannabinoid": "Alpha Pinene",
+            "Property": "Anti-Microbial",
+            "Score": 0.028,
+            "Percentage": 0.0004
+        },
+        {
+            "Category": "Property",
+            "TC": "Terpene",
+            "TerpeneCannabinoid": "Beta-Myrcene",
+            "Property": "Anti-Microbial",
+            "Score": 0.063,
+            "Percentage": 0.0009
+        },
+        {
+            "Category": "Property",
+            "TC": "Terpene",
+            "TerpeneCannabinoid": "cis-nerolidol",
+            "Property": "Anti-Microbial",
+            "Score": 0.028,
+            "Percentage": 0.0004
+        }
+    ]
+    */
     const responsive = {
         superLargeDesktop: {
             // the naming can be any, depends on you.
@@ -29,15 +171,55 @@ export default function Results({ SearchID, runResponseList, analysisResponse, t
         }
     };
 
+    //const [addJSON, setSampleAddJSON] = useState({})
+    //const [runResponseList, setResultsJSON] = useState({})
+    //const [analysisResponse, setAnalysisJSON] = useState({})
+    let loading = false
+    //const [loading, setLoading] = useState(false);
+    /*useEffect(() => {
+        console.log(searchRunRequestBody)
+        console.log(searchRunRequestBody.SearchID)
+        console.log(searchRunRequestBody[0])
+        async function getStrainTypes() {
+            try {
+                const res = await axios.post
+                    (
+                        'https://ss.prestoapi.com/api/v1_searchrun',
+                        searchRunRequestBody,
+                        {
+                            headers:
+                            {
+                                'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJmZTgzNGE4Yy04ZDM4LTRhODgtYmZkYi0yMTgyZDA1OTYxZWYiLCJwcm9qZWN0SUQiOiIzNTEwIiwibmFtZSI6IkphdmFzY3JpcHRUZXN0ZXIiLCJlbWFpbCI6ImphdmFzY3JpcHRAZ29vZ2xlLmNvbSIsInByb3ZpZGVyIjoiRW1haWwvUGFzc3dvcmQiLCJjcmVhdGVkIjoiMS80LzIwMjMgODo1NToxOCBQTSIsImxhc3RMb2dpbiI6IjMvMjYvMjAyMyA5OjAzOjMzIFBNIiwicm9sZSI6IkNyZWF0ZSIsIm1ldGFkYXRhIjoie1wiXCI6XCJcIn0iLCJleHAiOjE2Nzk4ODEzNjUsImlzcyI6IlByZXN0b0FQSSIsImF1ZCI6InNzIn0.z9mJT5JGDnwRcN2GX8WAVS1jLqYwMxBHIXtWdsbZFCI',
+                                'Content-Type': 'application/json'
+                            }
+                        }
+                    );
+                setAnalysisJSON(res.data);
+                setLoading(true);
+                console.log(strainTypes);
+            } catch (err) {
+                console.log(err)
+            }
+        } 
+        getStrainTypes();
+    }
+
+        , []);
+        */
 
     return (
         <div className="container main-container opacity">
-            <h1 className="headerTxt purple"></h1>
-            {console.log('analysisResponse from Results Component', analysisResponse)}
-            {console.log('Run ResponseList from Results Component', runResponseList)}
+            <h1 className="headerTxt purple">Results</h1>
             <div>
-                <p>Based on your responses, you will benefit most from the folowing terpenes:</p>
-                    <ul>{analysisResponse.map((results) => (<li>{results.TerpeneCannabinoid} for {results.Property}</li>))}</ul>
+                {/*console.log('Run ResponseList from Results Component', resultsList)*/}
+                {console.log('Run ResponseList from Results Component', runResponseList)}
+                <h2>Submission Number</h2>
+                <h1>{SearchID}</h1>
+            </div>
+            <div>
+                <h2>Suggestion Details</h2>
+                <p>Based on your responses, you will benefit most from the folowing terpenes:
+                    {analysisResponse.map((results) => (<span>{results.TerpeneCannabinoid} </span>))}</p>
             </div>
             {runResponseList.map((response) => (                       
                             <div>
